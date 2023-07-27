d835c7cd605e26c954b354b26b1327f7e1c2a19c
Cecille-SheCodes <137907754+Cecille-SheCodes@users.noreply.github.com>
2023-07-27 14:02:41 +0200

    Added the JS and CSS for the featured locations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-alert("hello");
+//Berlin
+function updateTime(){
+let berlinElement = document.querySelector("#berlin");
+let berlinDateElement= berlinElement.querySelector(".date");
+let berlinTimeElement = berlinElement.querySelector(".time");
+berlinDateElement.innerHTML = moment().tz("Europe/Berlin").format("dddd, MMMM D, YYYY");
+berlinTimeElement.innerHTML = moment()
+  .tz("Europe/Berlin")
+  .format("h:mm:ss [<small>]A[</small>]");
+
+
+
+//Manila
+
+let manilaElement = document.querySelector("#manila");
+let manilaDateElement = manilaElement.querySelector(".date");
+let manilaTimeElement = manilaElement.querySelector(".time");
+manilaDateElement.innerHTML = moment()
+  .tz("Asia/Manila")
+  .format("dddd, MMMM D, YYYY");
+manilaTimeElement.innerHTML = moment()
+  .tz("Asia/Manila")
+  .format("h:mm:ss [<small>]A[</small>]");
+
+
+
+  // New York
+
+  let nyElement = document.querySelector("#new-york");
+  let nyDateElement = nyElement.querySelector(".date");
+  let nyTimeElement = nyElement.querySelector(".time");
+  nyDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("dddd, MMMM D, YYYY");
+  nyTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
